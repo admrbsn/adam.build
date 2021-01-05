293507ee4b8fcf0407c103ba69efee53fe03c5a7
admrbsn <admrbsn@gmail.com>
2021-01-05 15:07:39 -0700

    Adding case studies.

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -467,7 +467,7 @@
               Recent work
             </h4>
             <p class="text-gray-700">
-              Case studies are in the works. In the meantime, check out what I've been up to on Dribbble.
+              Check out what I've been up to on Dribbble. Or, check out a case study on Medium.
             </p>
             <a
               href="https://dribbble.com/adamrobertson/"
@@ -486,6 +486,25 @@
                 />
               </svg>
               <span>View my Dribbble</span>
+            </a>
+
+            <a
+              href="https://adamrobertson.medium.com/"
+              target="_blank"
+              class="inline-flex items-center mr-2 py-4 px-6 bg-white text-black hover:text-black border border-gray-400 hover:border-black rounded shadow font-medium"
+            >
+              <svg
+              class="w-5 mr-2 fill-current"
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 1043.63 592.71"
+              >
+                <path
+                  d="M588.67 296.36c0 163.67-131.78 296.35-294.33 296.35S0 460 0 296.36 131.78 0 294.34 0s294.33 132.69 294.33 296.36M911.56 296.36c0 154.06-65.89 279-147.17 279s-147.17-124.94-147.17-279 65.88-279 147.16-279 147.17 124.9 147.17 279M1043.63 296.36c0 138-23.17 249.94-51.76 249.94s-51.75-111.91-51.75-249.94 23.17-249.94 51.75-249.94 51.76 111.9 51.76 249.94"
+                >
+              </svg>
+              <span>View case studies</span>
             </a>
           </div>
         </div>
